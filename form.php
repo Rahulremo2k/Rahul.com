e8c5d4b816b25dcf6f07b188a023b5b60575e316
Rahulremo2K <rahulduraisamy2@gmail.com>
2020-07-07 06:52:20 +0530

    changed some bg cantact forms

--- form.php
+++ form.php
@@ -1,24 +1,18 @@
 <?php
-    $name =$_POST['name'];
-    $visitor_email = $_POST['email'];
-    $maessage = $_POST['message'];
-
-    $email_form = 'rahulremo2000@gmail.com';
-
-    $email_subject = "New form Submission";
-
-    $email_body = "User Name: $name.\n"."User Email: $email.\n"."User Message: $message.\n";
-
-    $to = "rahulduraisamy2@gmail.com";
-
-    $headers = "From: $email_form \r\n";
-
-    $headers .= "Reply-To: $visitor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: index.html");
+$name =$_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];
 
 
+if(empty($name)  || empty($email) || empty($message))
+{
+    echo "Please fill all the fields";
+}
+else{
+    mail("rahulduraisamy2@gmail.com","From website",$message,"From: $name <$email>");
+    echo "<script type='text/javascript'> alert('Your message sent successfully');
+    window.history.log(-1); 
+    </script> ";
+}
 
-?>  
+?>
